653f68f5f0b16c2f7b013fd9a3786711e2f421d1
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2022-12-12 17:07:48 +0700

    menambahkan display

--- Queues_020/Program.cs
+++ Queues_020/Program.cs
@@ -48,6 +48,32 @@ namespace Queues_020
         }
         public void remove()
         {
+            /* CHecks wheather the queue is empty. */
+            if (FRONT == -1)
+            { 
+                Console.WriteLine("Queue underflow\n");
+                return ;
+            }
+            Console.WriteLine("\nThe Element deleted from the queue is: " + queue_array[FRONT] + "\n");
+            /* Check if the queue has one element. */
+            if (FRONT == REAR)
+            {
+                FRONT = -1;
+                REAR = -1;
+            }
+            else
+            {
+                /* If the element to be deleted is at the last position of the array, then the value 
+                 * of FRONT is set to 0 i.e to the first element of the array. */
+                if (FRONT == max - 1)
+                    FRONT = 0;
+                else
+                    /*FRONT is increnebted by one if it is not the first element of array. */
+                    FRONT = FRONT + 1;
+            }
+        }
+        public void display()
+        {
 
         }
         static void Main(string[] args)
