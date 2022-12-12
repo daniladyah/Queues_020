d29a5484ce8d72057eeff5ec8080f5a67794282e
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2022-12-12 16:41:46 +0700

    memasukkan front and rear

--- Queues_020/Program.cs
+++ Queues_020/Program.cs
@@ -8,7 +8,16 @@ namespace Queues_020
 {
     class Program
     {
-        int 
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+
+       public Program()
+        {
+            /*Initializing the values of the variables REAR and FRONT to -1 to indicate
+             * the queue is initially empty.*/
+            FRONT = -1;
+            REAR = -1;
+        }
         static void Main(string[] args)
         {
         }
