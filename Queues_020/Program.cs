a55272026af3db4ee3462ac639df0fa271854e7d
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2022-12-12 16:32:40 +0700

    Add project files.

---
+++ Queues_020/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues_020
+{
+    class Program
+    {
+        int 
+        static void Main(string[] args)
+        {
+        }
+    }
+}
